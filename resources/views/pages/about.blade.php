eae57cdeace82a64f7f93cfcca17eff0d846a590
Samankassou <samankassoufoulla@gmail.com>
2020-11-01 14:21:46 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Hello city</title>
-    </head>
-    <body>
-        <p>Built with &hearts; by SAMANKASSOU FOULLA</p>
-        <p><a href="/">Revenir à la page d'accueil</a></p>
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }}</p>
-        </footer>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <p>Built with &hearts; by SAMANKASSOU FOULLA</p>
+    <p><a href="/">Revenir à la page d'accueil</a></p>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts/app')
 
-        <title>About us | Hello city</title>
-    </head>
-    <body>
-        <h1>Hello from Cameroon!</h1>
-        <p>It'is currently {{ date('h:i A') }}</p>
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About us</a></p>
-        </footer>
-    </body>
-</html>
-
+@section('content')
+    <h1>Hello from Cameroon!</h1>
+    <p>It'is currently {{ date('h:i A') }}</p>
+@endsection
