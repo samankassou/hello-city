e294328e4b0c071d08f777691c9acc625ee11739
Samankassou <samankassoufoulla@gmail.com>
2020-11-01 14:10:47 +0100

    Move static pages to page folder and remove link from about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,7 +10,7 @@
         <p>Built with &hearts; by SAMANKASSOU FOULLA</p>
         <p><a href="/">Revenir à la page d'accueil</a></p>
         <footer>
-            <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About us</a></p>
+            <p>&copy; Copyright {{ date('Y') }}</p>
         </footer>
     </body>
 </html>
