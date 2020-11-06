7798360f346babb5025b6a679f0a5796835fe4f0
Samankassou <samankassoufoulla@gmail.com>
2020-11-06 02:00:18 +0100

    Replace TDN Logo with my own picture

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'About Us | '.config('app.name'))
 @section('content')
-	<img src="{{ asset('images/tdn-logo.png') }}" alt="TDN Logo" class="my-12 rounded-full shadow-md">
+	<img src="{{ asset('images/sam-img.png') }}" alt="TDN Logo" class="my-12 rounded-full shadow-md">
 
     <h2 class="mb-5 text-gray-700">
     	Built with <span class="text-pink-500">&hearts;</span> by SAMANKASSOU FOULLA
